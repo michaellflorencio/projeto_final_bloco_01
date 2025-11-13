19580d47ce61502c0832ec17d43093177c48b278
michaellflorencio <michael.lima.florencio@gmail.com>
2025-11-13 15:56:59 -0300

    Construção da Classe Controller e Métodos do CRUD

--- Menu.ts
+++ Menu.ts
@@ -1,6 +1,10 @@
 import readlinesync = require("readline-sync");
 import { Camiseta } from "./src/model/Camiseta";
+import { ProdutoController } from "./src/controller/ProdutoController";
+import { Produto } from "./src/model/Produto";
+
 export function main() {
+    let produto: ProdutoController = new ProdutoController();
 
     let opcao: number;
 
@@ -18,10 +22,8 @@ export function main() {
         console.log("4 - Apagar Produto");
         console.log("5 - Sair");
         console.log("                                                     ");
-
         console.log("*****************************************************");
         console.log("                                                     ");
-
         console.log("Entre com a opção desejada: ");
         opcao = readlinesync.questionInt("");
 
@@ -38,25 +40,31 @@ export function main() {
                 const nome = readlinesync.question("Nome: ");
                 const preco = readlinesync.questionFloat("Preço: ");
                 const tamanho = readlinesync.question("Tamanho: ");
-                const produto = new Camiseta(id, nome, preco, tamanho);
-                console.log("\nProduto cadastrado com sucesso!");
-                produto.visualizar();
-
+                const novoProduto = new Camiseta(id, nome, preco, tamanho);
+                produto.cadastrar(novoProduto); // salva no controller
                 break;
+
             case 2:
                 console.log("\n\nListar todos os Produtos\n\n");
-
+                produto.listarTodos(); // mostra produtos cadastrados
                 break;
+
             case 3:
-                console.log("\n\nListar Produtos por ID\n\n");
-
+                console.log("\n\nListar Produto por ID\n\n");
+                const idBusca = readlinesync.questionInt("Digite o ID: ");
+                produto.buscarPorId(idBusca); // busca produto específico
                 break;
+
             case 4:
                 console.log("\n\nApagar Produto\n\n");
-
+                const idDel = readlinesync.questionInt("Digite o ID: ");
+                produto.deletar(idDel); // remove produto da lista
                 break;
+
             case 5:
                 console.log("\n\nSair\n\n");
+                sobre();
+                process.exit(0);
 
             default:
                 console.log("\nOpção Inválida!\n");
---
+++ src/controller/ProdutoController.ts
@@ -0,0 +1,73 @@
+import { Produto } from "../model/Produto";
+import { ProdutoRepository } from "../repository/ProdutoRepository";
+
+export class ProdutoController implements ProdutoRepository {
+
+    private listaProdutos: Array<Produto> = new Array<Produto>();
+    private id: number = 0;
+
+
+    public cadastrar(produto: Produto): void {
+        this.listaProdutos.push(produto);
+        console.log("\n✅ Produto cadastrado com sucesso!");
+    }
+
+
+    public listarTodos(): void {
+        if (this.listaProdutos.length === 0) {
+            console.log("\n Nenhum produto cadastrado!");
+            return;
+        }
+
+        for (let produto of this.listaProdutos) {
+            produto.visualizar();
+        }
+    }
+
+    public buscarPorId(id: number): void {
+        const produto = this.buscarNoArray(id);
+
+        if (produto != null) {
+            produto.visualizar();
+        } else {
+            console.log("\n Produto com ID: " + id + " não foi encontrado!");
+        }
+    }
+
+
+    public atualizar(produto: Produto): void {
+        const buscaProduto = this.buscarNoArray(produto.getId());
+
+        if (buscaProduto != null) {
+            this.listaProdutos[this.listaProdutos.indexOf(buscaProduto)] = produto;
+            console.log("\n Produto ID " + produto.getId() + " atualizado com sucesso!");
+        } else {
+            console.log("\n Produto ID " + produto.getId() + " não encontrado!");
+        }
+    }
+
+
+    public deletar(id: number): void {
+        const buscaProduto = this.buscarNoArray(id);
+
+        if (buscaProduto != null) {
+            this.listaProdutos.splice(this.listaProdutos.indexOf(buscaProduto), 1);
+            console.log("\n Produto ID " + id + " deletado com sucesso!");
+        } else {
+            console.log("\n Produto ID " + id + " não encontrado!");
+        }
+    }
+
+    public gerarId(): number {
+        return ++this.id;
+    }
+
+    private buscarNoArray(id: number): Produto | null {
+        for (let produto of this.listaProdutos) {
+            if (produto.getId() === id) {
+                return produto;
+            }
+        }
+        return null;
+    }
+}
